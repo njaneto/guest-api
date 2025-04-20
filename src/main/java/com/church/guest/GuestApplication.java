abf538db2875b256eeca5ea4bfb7092285db159e
Nilo Jose de Andrade Neto <nneto.kstack@veloe.com.br>
2025-04-19 22:15:03 -0300

    add websocket

--- src/main/java/com/church/guest/GuestApplication.java
+++ src/main/java/com/church/guest/GuestApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@SpringBootApplication(scanBasePackages = "com.church")
 @EnableMongoAuditing
+@SpringBootApplication
 public class GuestApplication {
 
 	public static void main(String[] args) {
