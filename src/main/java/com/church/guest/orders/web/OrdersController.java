d83b9e64f7de5f5e15f963787d6e5b5dd75745f6
Nilo Jose de Andrade Neto <nneto.kstack@veloe.com.br>
2025-09-14 04:43:50 -0300

    add whats

--- src/main/java/com/church/guest/orders/web/OrdersController.java
+++ src/main/java/com/church/guest/orders/web/OrdersController.java
@@ -51,19 +51,19 @@ public class OrdersController {
         return OrdersMapper.toOrderResponses( responses );
     }
 
-    @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE )
-    @ResponseStatus( value = HttpStatus.OK )
-    @Secured( "CJ_ROLE_USER_READ" )
-    public OrdersResponse findAllOrdersByOrder(@RequestParam String numeroPedido) {
-
-        final List< OrderDTO > responses = service.findAllByNumeroPedido(numeroPedido)
-                .stream()
-                .sorted( Comparator.comparing( Order ::getStatusPagamento ).reversed())
-                .map( OrdersMapper :: toOrderDTO )
-                .toList();
-
-        return OrdersMapper.toOrderResponses( responses );
-    }
+//    @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE )
+//    @ResponseStatus( value = HttpStatus.OK )
+//    @Secured( "CJ_ROLE_USER_READ" )
+//    public OrdersResponse findAllOrdersByOrder(@RequestParam String numeroPedido) {
+//
+//        final List< OrderDTO > responses = service.findAllByNumeroPedido(numeroPedido)
+//                .stream()
+//                .sorted( Comparator.comparing( Order ::getStatusPagamento ).reversed())
+//                .map( OrdersMapper :: toOrderDTO )
+//                .toList();
+//
+//        return OrdersMapper.toOrderResponses( responses );
+//    }
 
 
     @PutMapping( value = "/{id}/confirm" )
